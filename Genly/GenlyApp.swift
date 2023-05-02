bdb85e19d370288e4dc5ba1258da05e38dc1b107
Toni K. Turk <kocjanturk@gmail.com>
2023-05-02 16:13:21 +0200

    Initial Commit

---
+++ Genly/GenlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenlyApp.swift
+//  Genly
+//
+//  Created by Toni K. Turk on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct GenlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
